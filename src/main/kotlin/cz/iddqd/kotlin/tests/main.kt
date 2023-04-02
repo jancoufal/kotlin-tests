ff6e6376c49c41fd494c156322c32df0cf78b6f1
Jan Coufal <jan.coufal@gmail.com>
2023-04-02 16:02:38 +0200

    reactive streams

--- src/main/kotlin/cz/iddqd/kotlin/tests/main.kt
+++ src/main/kotlin/cz/iddqd/kotlin/tests/main.kt
@@ -3,24 +3,23 @@ package cz.iddqd.kotlin.tests
 import cz.iddqd.kotlin.tests.units.ReactiveStreams
 
 fun main() {
-    println("Starting...")
+	println("Starting...")
 
-    val tests = listOf<UnitTest>(
-        ReactiveStreams()
-    )
+	val tests = listOf<UnitTest>(
+		ReactiveStreams()
+	)
 
-    tests.forEach {
-        println("Running ${it.javaClass}...")
-        try {
-            it.go()
-            println("...ok")
-        }
-        catch (e: Exception) {
-            println("...failed due to ${e.javaClass}: ${e.message}")
-        }
-    }
+	tests.forEach {
+		println("Running ${it.javaClass.simpleName}...")
+		try {
+			it.go()
+			println("...ok")
+		} catch (e: Exception) {
+			println("...failed due to ${e.javaClass.simpleName}: ${e.message}")
+		}
+	}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("done.")
+	// Try adding program arguments via Run/Debug configuration.
+	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+	println("done.")
 }
--- src/main/kotlin/cz/iddqd/kotlin/tests/units/ReactiveStreams.kt
+++ src/main/kotlin/cz/iddqd/kotlin/tests/units/ReactiveStreams.kt
@@ -1,9 +1,54 @@
 package cz.iddqd.kotlin.tests.units
 
 import cz.iddqd.kotlin.tests.UnitTest
+import java.time.Duration
+import java.util.concurrent.Flow
+import java.util.concurrent.Flow.Subscriber
+import java.util.concurrent.SubmissionPublisher
 
-class ReactiveStreams: UnitTest {
-    override fun go() {
-        TODO("Not yet implemented")
-    }
+class ReactiveStreams : UnitTest {
+	override fun go() {
+		val publisher = SubmissionPublisher<Int>()
+
+		publisher.subscribe(Reader("Inky", -1))
+		publisher.subscribe(Reader("Blinky", 0))
+		publisher.subscribe(Reader("Pinky", 1))
+		publisher.subscribe(Reader("Clyde", 2))
+		publisher.subscribe(Reader("PacMan", Long.MAX_VALUE))
+
+		for(i in 0..5) {
+			println("Publishing $i")
+			publisher.submit(i)
+		}
+
+		publisher.close()
+
+		val sleepDuration = Duration.ofSeconds(1L)
+		println("Waiting $sleepDuration to complete.")
+		Thread.sleep(sleepDuration.toMillis())
+
+		println("Publishing -1 after close")
+		publisher.submit(-1)
+	}
+
+	class Reader(private val name: String, private val request: Long) : Subscriber<Int> {
+
+		override fun onSubscribe(subscription: Flow.Subscription?) {
+			println("$name.onSubscribe(subscription=$subscription)")
+			println("$name.request($request)")
+			subscription?.request(request)
+		}
+
+		override fun onNext(item: Int?) {
+			println("$name.onNext(item=$item)")
+		}
+
+		override fun onError(throwable: Throwable?) {
+			println("$name.onError(throwable=$throwable)")
+		}
+
+		override fun onComplete() {
+			println("$name.onComplete()")
+		}
+	}
 }
