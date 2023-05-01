f9d1445362090fdb28d7e46bcb03ad22b0438b82
Jan Coufal <jan.coufal@gmail.com>
2023-05-01 15:00:10 +0200

    CoRoutine1

--- src/main/kotlin/cz/iddqd/kotlin/tests/main.kt
+++ src/main/kotlin/cz/iddqd/kotlin/tests/main.kt
@@ -6,9 +6,10 @@ fun main() {
 	println("Starting...")
 
 	val tests = listOf(
-		ReactiveStreams(),
-		ReactiveStreamsParallel(),
-		ThreadSafeSingleton()
+//		ReactiveStreams(),
+//		ReactiveStreamsParallel(),
+//		ThreadSafeSingleton(),
+		CoRoutine1(),
 	)
 
 	tests.forEach {
@@ -20,6 +21,10 @@ fun main() {
 			println("...failed due to ${e.javaClass.simpleName}: ${e.message}")
 		}
 	}
+
+	// to test the coroutines
+	println("sleeping 1 sec")
+	Thread.sleep(1000L)
 
 	// Try adding program arguments via Run/Debug configuration.
 	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
---
+++ src/main/kotlin/cz/iddqd/kotlin/tests/units/CoRoutine1.kt
@@ -0,0 +1,19 @@
+package cz.iddqd.kotlin.tests.units
+
+import cz.iddqd.kotlin.tests.UnitTest
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class CoRoutine1 : UnitTest {
+	override fun go() {
+
+		// probably won't be printed out as the main thread is finished before the coroutine is executed
+		GlobalScope.launch {
+			delay(500L)
+			println("CoRoutine1 coroutine in GlobalScope from thread '${Thread.currentThread().name}'")
+		}
+
+		println("CoRoutine1 go() finished")
+	}
+}
