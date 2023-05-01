0a7780ae98a837aabfa4f4b37a70ab01331c79ee
Jan Coufal <jan.coufal@gmail.com>
2023-05-01 15:14:09 +0200

    CoRoutine2

--- src/main/kotlin/cz/iddqd/kotlin/tests/units/CoRoutine2.kt
+++ src/main/kotlin/cz/iddqd/kotlin/tests/units/CoRoutine2.kt
@@ -9,14 +9,22 @@ import kotlinx.coroutines.launch
 class CoRoutine2 : UnitTest {
 	override fun go() {
 		GlobalScope.launch {
-			doNetworkCall()
+			val networkCallAnswer1 = doNetworkCall1()
+			val networkCallAnswer2 = doNetworkCall2()
+			println("CoRoutine2::doNetworkCall1() running in '${Thread.currentThread().name}' thread returned '$networkCallAnswer1'")
+			println("CoRoutine2::doNetworkCall2() running in '${Thread.currentThread().name}' thread returned '$networkCallAnswer2'")
 		}
 
 		println("CoRoutine2::go() finished")
 	}
 
-	private suspend fun doNetworkCall() {
-		delay(500L)
-		println("CoRoutine2::doNetworkCall() finished in thread '${Thread.currentThread().name}'")
+	private suspend fun doNetworkCall1(): String {
+		delay(300L)
+		return "CoRoutine2::doNetworkCall1() answer"
+	}
+
+	private suspend fun doNetworkCall2(): String {
+		delay(300L)
+		return "CoRoutine2::doNetworkCall2() answer"
 	}
 }
